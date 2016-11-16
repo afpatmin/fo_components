b1e67038c07f5e0776e218eb3860cbbd9c432904
afpatmin <patrick.minogue@appfoundry.se>
2016-11-16 17:56:18 +0100

    0.0.111

---
+++ READMORE.md
@@ -0,0 +1,10 @@
+<h1>FoComponents 0.0.1</h1>
+<h2>fo-icon</h2>
+<p>Description: SVG icon component</p>
+<p>Attributes</p>
+<ul>
+    <li>foSrc: Which icon id from within [icons.svg]</li>
+    <li>foTitle: Icon title and aria-label 
+</ul>
+<p>Example</p>
+<code>&lt;fo-icon foSrc="coins" foTitle="Coins"&gt;&lt;/fo-icon&gt;</code>
---
+++ lib/components/icon_component.dart
@@ -0,0 +1,51 @@
+// Copyright (c) 2016, Muienog AB. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import 'dart:html';
+import 'package:angular2/core.dart';
+
+@Component(
+    selector: 'fo-icon',
+    styleUrls: const ['css/icon_component.css'],
+    templateUrl: 'html/icon_component.html',
+    directives: const [IconComponent, SourcedDirective, TitledDirective])
+
+class IconComponent
+{
+  IconComponent();
+  @Input() String foSrc = "clock";
+  @Input() String foTitle = "Icon";
+}
+
+@Directive(selector: '[foSourced]')
+class SourcedDirective implements OnInit
+{
+  SourcedDirective(ElementRef element) : _host = element.nativeElement;
+
+  void ngOnInit()
+  {
+    _host.setAttributeNS("http://www.w3.org/1999/xlink", "href", "gfx/icons/icons.svg#icon-" + src);
+  }
+
+  @Input('foSrc')
+  String src;
+
+  Element _host;
+}
+
+@Directive(selector: '[foTitled]')
+class TitledDirective implements OnInit
+{
+  TitledDirective(ElementRef element) : _host = element.nativeElement;
+
+  void ngOnInit()
+  {
+    _host.title = title;
+    _host.setAttribute("aria-label", title);
+  }
+
+  @Input('foTitle')
+  String title;
+
+  Element _host;
+}
