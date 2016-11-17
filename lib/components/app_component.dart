995c9553935a1fdf8ab533c591c4b1d6cedbff81
afpatmin <patrick.minogue@appfoundry.se>
2016-11-17 16:35:01 +0100

    latest

--- lib/components/app_component.dart
+++ lib/components/app_component.dart
@@ -2,7 +2,7 @@
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 import 'package:angular2/core.dart';
-import 'package:FoComponents/components/icon_component.dart';
+import 'package:fo_components/components/icon_component.dart';
 
 
 @Component(
--- web/main.dart
+++ web/main.dart
@@ -2,7 +2,7 @@
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 import 'package:angular2/platform/browser.dart';
-import 'package:FoComponents/components/app_component.dart';
+import 'package:fo_components/components/app_component.dart';
 
 main()
 {
