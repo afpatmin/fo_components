4c9d1ee115577fd611686c08a1e7395fccf88dc4
afpatmin <patrick.minogue@appfoundry.se>
2017-08-22 14:25:54 +0200

    modal and youtube fixes

--- lib/src/components/fo_youtube_player_component/fo_youtube_player_component.dart
+++ lib/src/components/fo_youtube_player_component/fo_youtube_player_component.dart
@@ -10,16 +10,22 @@ typedef void YoutubeCallback(dynamic event);
     styleUrls: const ['fo_youtube_player_component.css'],
     templateUrl: 'fo_youtube_player_component.html'
 )
-class FoYouTubePlayerComponent implements AfterContentInit, OnDestroy
+class FoYouTubePlayerComponent implements AfterContentInit, OnChanges, OnDestroy
 {
   FoYouTubePlayerComponent();
 
   void ngAfterContentInit()
   {
-    ScriptElement script = document.createElement('script');
-    script.src = "https://www.youtube.com/iframe_api";
-    document.head.children.add(script);
+    document.head.children.add(new ScriptElement()..src = "https://www.youtube.com/iframe_api");
     context['onYouTubeIframeAPIReady'] = _onAPIReady;
+  }
+
+  void ngOnChanges(Map<String, SimpleChange> changes)
+  {
+    if (changes.containsKey("videoId") && _player != null)
+    {
+      _player.callMethod("cueVideoById", [videoId]);
+    }
   }
 
   void ngOnDestroy()
@@ -32,23 +38,7 @@ class FoYouTubePlayerComponent implements AfterContentInit, OnDestroy
     /**
      * Youtube API is ready, initialize video
      */
-    Map<String, String> vars = new Map();
-    vars["fs"] = "1";
-    vars["rel"] = "0";                // Show related videos at the end of playback
-    vars["modestbranding"] = "1";     // Show minimal youtube branding
-    vars["origin"] = Uri.base.origin;
-    vars["enablejsapi"] = "1";
-    vars["autoplay"] = autoplay ? "1" : "0";
-    Map<String, YoutubeCallback> events = new Map();
-    events["onReady"] = _onReady;
-    events["onStateChange"] = _onStateChange;
-    Map<String, dynamic> params = new Map();
-    params["videoId"] = videoId;
-    params["playerVars"] = vars;
-    params["events"] = events;
-
-    JsObject jsParams = new JsObject.jsify(params);
-    /*JsObject player = */ new JsObject(context['YT']['Player'], ['youtube-player-video', jsParams]);
+    _player = new JsObject(context['YT']['Player'], ['youtube-player-video', params]);
     
   }
 
@@ -85,7 +75,29 @@ class FoYouTubePlayerComponent implements AfterContentInit, OnDestroy
     }
   }
 
+  JsObject get params
+  {
+    Map<String, String> vars = new Map();
+    vars["fs"] = "1";
+    vars["rel"] = "0";                // Show related videos at the end of playback
+    vars["modestbranding"] = "1";     // Show minimal youtube branding
+    vars["origin"] = Uri.base.origin;
+    vars["enablejsapi"] = "1";
+    vars["autoplay"] = autoplay ? "1" : "0";
+    Map<String, YoutubeCallback> events = new Map();
+    events["onReady"] = _onReady;
+    events["onStateChange"] = _onStateChange;
+    Map<String, dynamic> params = new Map();
+    params["videoId"] = videoId;
+    params["playerVars"] = vars;
+    params["events"] = events;
+
+    return new JsObject.jsify(params);
+  }
+
   final StreamController<String> _onStateChangeController = new StreamController();
+
+  JsObject _player;
 
   @Input('videoId')
   String videoId;
