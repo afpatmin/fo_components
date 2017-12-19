39794a1a6d685ae10a7964aae4c6a267ed3e6daa
afpatmin <patrick.minogue@appfoundry.se>
2017-12-19 21:45:58 +0100

    stop printing foSelect::selectedId

--- lib/src/components/fo_select_component/fo_select_component.dart
+++ lib/src/components/fo_select_component/fo_select_component.dart
@@ -84,8 +84,6 @@ class FoSelectComponent implements OnDestroy
   @Input('selectedId')
   void set selectedId(String value)
   {
-
-    print("selectedId:$value");
     _selectionChangeListener.cancel();
 
     if (value == null) selectionModel.clear();
