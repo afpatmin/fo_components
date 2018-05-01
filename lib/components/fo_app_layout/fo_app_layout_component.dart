d209b45cd68df85e21d44e459a83e3bbb0ec07af
Patrick Minogue <patrick.minogue@gmail.com>
2018-05-01 10:36:54 +0200

    Made routing internal to AppLayoutComponent

--- lib/components/fo_app_layout/fo_app_layout_component.dart
+++ lib/components/fo_app_layout/fo_app_layout_component.dart
@@ -4,6 +4,7 @@
 import 'dart:async';
 import 'dart:html' as html;
 import 'package:angular/angular.dart';
+import 'package:angular_router/angular_router.dart';
 import 'package:angular/security.dart' as security;
 import 'package:angular_components/angular_components.dart';
 import '../../pipes/phrase_pipe.dart';
@@ -12,38 +13,28 @@ import '../fo_modal/fo_modal_component.dart';
 
 @Component(
     selector: 'fo-app-layout',
-    styleUrls: const ['fo_app_layout_component.css'],
+    styleUrls: ['fo_app_layout_component.css'],
     templateUrl: 'fo_app_layout_component.html',
-    directives: const [coreDirectives, FoModalComponent, materialDirectives],
-    pipes: const [PhrasePipe],
-    visibility: Visibility.local)
-class FoAppLayoutComponent implements OnInit, OnDestroy {
-  FoAppLayoutComponent(this.phraseService, this._domSanitizationService);
-
-  @override
-  void ngOnInit() {
-    if (categories.isNotEmpty && categories.first.items.isNotEmpty) {
-      pageIcon = categories.first.items.first.icon;
-      pageHeader = categories.first.items.first.label;
-    }
+    directives: [coreDirectives, FoModalComponent, materialDirectives],
+    pipes: [PhrasePipe])
+class FoAppLayoutComponent implements OnDestroy {
+  FoAppLayoutComponent(
+      this.router, this.phraseService, this._domSanitizationService) {
+    
+    router.onRouteActivated.listen((state) {
+      _activeItem = null;
+      
+      for (final category in categories) {
+        _activeItem = category.items
+            .firstWhere((i) => i.url == state.path, orElse: () => null);
+        if (_activeItem != null) break;
+      }
+    });
   }
 
   @override
   void ngOnDestroy() {
-    _onExpandedChangeController.close();
-    _onNavigateController.close();
-    _onInstructionsCloseController.close();
-  }
-
-  void onNavigate(FoSidebarItem item) {
-    _activeItem = item;
-    pageIcon = item.icon;
-    pageHeader = item.label;
-    instructionsUrl = item.instructionsUrl == null
-        ? null
-        : _domSanitizationService
-            .bypassSecurityTrustResourceUrl(item.instructionsUrl);
-    _onNavigateController.add(item.url);
+    _onExpandedChangeController.close();    
   }
 
   void toggleExpanded() {
@@ -55,13 +46,6 @@ class FoAppLayoutComponent implements OnInit, OnDestroy {
     _onExpandedChangeController.add(expanded);
   }
 
-  void onInstructionsModalVisibleChange(bool visible) {
-    if (!visible) {
-      _onInstructionsCloseController.add(instructionsUrl.toString());
-    }
-    instructionsModalVisible = visible;
-  }
-
   bool isActive(FoSidebarItem item) => item == _activeItem;
 
   String calcIFrameWidth() => (html.window.innerWidth * 0.6).toString();
@@ -71,18 +55,22 @@ class FoAppLayoutComponent implements OnInit, OnDestroy {
 
   String get sidebarWidth => (expanded) ? '${width}px' : '${miniWidth}px';
 
+  String get pageHeader => _activeItem?.label;
+  String get pageIcon => _activeItem?.icon;
+  security.SafeResourceUrl get instructionsUrl =>
+      _activeItem.instructionsUrl == null
+          ? null
+          : _domSanitizationService
+              .bypassSecurityTrustResourceUrl(_activeItem.instructionsUrl);
+
   final PhraseService phraseService;
   bool animating = false;
-  String pageHeader = '';
-  String pageIcon = '';
-  security.SafeResourceUrl instructionsUrl;
+
   bool instructionsModalVisible = false;
   final security.DomSanitizationService _domSanitizationService;
+  final Router router;
   final StreamController<bool> _onExpandedChangeController =
-      new StreamController();
-  final StreamController<String> _onNavigateController =
-      new StreamController();
-  final StreamController<String> _onInstructionsCloseController = new StreamController();
+      new StreamController();  
   final int miniWidth = 40;
   FoSidebarItem _activeItem;
 
@@ -104,28 +92,8 @@ class FoAppLayoutComponent implements OnInit, OnDestroy {
   @Input()
   List<FoSidebarCategory> categories = <FoSidebarCategory>[];
 
-  @Input()
-  // ignore: avoid_setters_without_getters
-  set instructions(String value) {
-    if (value == null) {
-      instructionsUrl = null;
-      instructionsModalVisible = false;
-    }
-    else {
-      instructionsUrl = _domSanitizationService
-          .bypassSecurityTrustResourceUrl(value);
-      instructionsModalVisible = true;
-    }
-  }
-
-  @Output('instructionsClose')
-  Stream<String> get instructionsClose => _onInstructionsCloseController.stream;
-
   @Output('expandedChange')
   Stream<bool> get onExpandedChangeOutput => _onExpandedChangeController.stream;
-
-  @Output('navigate')
-  Stream<String> get onNavigateOutput => _onNavigateController.stream;
 }
 
 class FoSidebarCategory {
