b550e8fbc54bd7d41d94cead32f70f8430b0639d
Patrick Minogue <patrick.minogue@gmail.com>
2018-05-09 14:52:57 +0200

    Use selectionModel instead of for loop

--- lib/components/fo_select/fo_select_component.dart
+++ lib/components/fo_select/fo_select_component.dart
@@ -13,8 +13,7 @@ import '../fo_modal/fo_modal_component.dart';
     styleUrls: const ['fo_select_component.css'],
     templateUrl: 'fo_select_component.html',
     directives: const [coreDirectives, materialDirectives, FoModalComponent],
-    pipes: const [PhrasePipe],
-    visibility: Visibility.local)
+    pipes: const [PhrasePipe])
 class FoSelectComponent implements OnChanges, OnDestroy {
   FoSelectComponent();
 
@@ -57,6 +56,10 @@ class FoSelectComponent implements OnChanges, OnDestroy {
       new StreamController();
   final StreamController<Object> onActionButtonTriggerController =
       new StreamController();
+
+  final SelectionModel selectionModel = new SingleSelectionModel();
+  
+  ItemRenderer<FoModel> itemRenderer = (FoModel model) => model.id?.toString();
 
   bool tooltipModalVisible = false;
 
