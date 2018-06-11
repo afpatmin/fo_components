c3410fb02339a8959b40b92238c7eaa3797eace3
Patrick Minogue <patrick.minogue@gmail.com>
2018-06-12 00:35:31 +0200

    Made sure name pipe skips empty string

--- lib/pipes/fo_name_pipe.dart
+++ lib/pipes/fo_name_pipe.dart
@@ -3,5 +3,5 @@ import 'package:angular/di.dart' show Pipe, PipeTransform;
 @Pipe('name')
 class NamePipe implements PipeTransform {
   String transform(String value) =>
-      value == null ? null : '${value[0].toUpperCase()}${value.substring(1)}';
+      value == null || value.isEmpty ? value : '${value[0].toUpperCase()}${value.substring(1)}';
 }
