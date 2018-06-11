ed82c574b1d66993c8450c541aaa3aabfe5d50b1
Patrick Minogue <patrick.minogue@gmail.com>
2018-06-11 23:42:35 +0200

    Created name pipe

---
+++ lib/pipes/fo_name_pipe.dart
@@ -0,0 +1,7 @@
+import 'package:angular/di.dart' show Pipe, PipeTransform;
+
+@Pipe('name')
+class NamePipe implements PipeTransform {
+  String transform(String value) =>
+      value == null ? null : '${value[0].toUpperCase()}${value.substring(1)}';
+}
