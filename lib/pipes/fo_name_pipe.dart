7c1dc1abaa998f008d043eb347ef20df1d660ec8
Patrick Minogue <patrick.minogue@gmail.com>
2018-06-14 09:27:37 +0200

    Added comment to name pipe

--- lib/pipes/fo_name_pipe.dart
+++ lib/pipes/fo_name_pipe.dart
@@ -1,5 +1,7 @@
 import 'package:angular/di.dart' show Pipe, PipeTransform;
 
+/// Makes the first letter of a string uppercase
+/// patrick --> Patrick
 @Pipe('name')
 class NamePipe implements PipeTransform {
   String transform(String value) =>
