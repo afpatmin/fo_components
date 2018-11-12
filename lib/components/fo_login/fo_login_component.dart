6446d9aeff89f1d1e95eccee37fc8b91da250782
Patrick Minogue <patrick.minogue@gmail.com>
2018-11-12 20:48:37 +0100

    Removed viauth services from FoLoginComponent

--- lib/components/fo_login/fo_login_component.dart
+++ lib/components/fo_login/fo_login_component.dart
@@ -5,48 +5,61 @@ import 'dart:async';
 import 'dart:html' as html;
 import 'package:angular/angular.dart';
 import 'package:angular_components/angular_components.dart';
-import 'package:vi_auth_client/vi_auth_client.dart';
 import '../../services/fo_messages_service.dart';
 import '../fo_modal/fo_modal_component.dart';
+
+class LoginEvent {
+  String username;
+  String password;
+}
+
+class UpdatePasswordEvent {
+  String username;
+  String password;
+  String token;
+}
 
 @Component(
     selector: 'fo-login',
     styleUrls: const ['fo_login_component.css'],
     templateUrl: 'fo_login_component.html',
-    directives: const [NgIf, materialInputDirectives, MaterialButtonComponent, FoModalComponent],
+    directives: const [
+      NgIf,
+      materialInputDirectives,
+      MaterialButtonComponent,
+      FoModalComponent
+    ],
     pipes: const [])
-class FoLoginComponent implements OnInit, OnDestroy {
+class FoLoginComponent implements OnDestroy {
   FoLoginComponent(this.msg) {
     state = msg.login();
   }
 
   @override
-  void ngOnInit() {
-    if (client != null &&
-        Uri.base.queryParameters.containsKey('token') &&
-        Uri.base.queryParameters.containsKey('username')) {
-      _tryLoginToken(Uri.decodeFull(Uri.base.queryParameters['username']),
-          Uri.base.queryParameters['token']);
-    }
-  }
-
-  @override
   void ngOnDestroy() {
     _onLoginController.close();
+    _onRecoverPasswordController.close();
+    _onUpdatePasswordController.close();
   }
 
-  Future onLogin() async {
-    try {
-      errorMessage = null;
-      final token = await client.loginWithUsernamePassword(username, password);
-      _onLoginController.add({'username': username, 'token': token});
-      visible = false;
-    } on Exception {
-      errorMessage = msg.invalid_details();
-    }
+  void onLogin() {
+    _onLoginController.add(new LoginEvent()
+      ..username = username
+      ..password = password);
   }
 
-  void onKeyUp(html.KeyboardEvent e) {
+  void onRecoverPassword() {
+    _onRecoverPasswordController.add(username);
+  }
+
+  void onUpdatePassword() {
+    _onUpdatePasswordController.add(new UpdatePasswordEvent()
+      ..username = username
+      ..password = password
+      ..token = token);    
+  }
+
+  void onLoginKeyUp(html.KeyboardEvent e) {
     if (username.isNotEmpty &&
         password.isNotEmpty &&
         (e.keyCode == html.KeyCode.ENTER ||
@@ -62,98 +75,34 @@ class FoLoginComponent implements OnInit, OnDestroy {
     }
   }
 
-  Future onRecoverPassword() async {
-    if (username.isNotEmpty) {
-      errorMessage = null;
-      try {
-        await client.sendCredentials(username,
-            emailFrom: recoverPasswordFromEmail,
-            emailMessage: recoverPasswordMessageEmail,
-            emailSubject: recoverPasswordSubjectEmail,
-            smsMessage: recoverPasswordMessageSMS,
-            smsFrom: recoverPasswordFromSMS);
-
-        recoverPasswordSent = true;
-        state = msg.reset_password();
-      } on Exception catch (e, s) {
-        print(s);
-        errorMessage = e.toString();
-      }
-    }
-  }
-
-  Future onUpdatePassword() async {
-    try {
-      errorMessage = null;
-      await client.updatePassword(username, password, token);
-      token = '';
-      setState(msg.login());
-    } on Exception catch (e, s) {
-      print(s);
-      errorMessage = e.toString();
-    }
-  }
-
   void setState(String newState) {
-    state = newState;
-    recoverPasswordSent = false;
+    state = newState;  
     errorMessage = null;
   }
 
-  Future _tryLoginToken(String username, String token) async {
-    try {
-      visible = false;
-      final newToken = await client.loginWithToken(token);
-      _onLoginController.add({'username': username, 'token': newToken});
-    } on Exception catch (e) {
-      print(e.toString());
-      visible = true;
-    }
-  }
-
   String token = '';
-
-  String state;
-  String errorMessage;
-  bool recoverPasswordSent = false;
+  String state;  
+  
   bool visible = true;
   final FoMessagesService msg;
-  final StreamController<Map<String, String>> _onLoginController =
+  final StreamController<LoginEvent> _onLoginController =
+      new StreamController();
+  final StreamController<String> _onRecoverPasswordController =
+      new StreamController();
+  final StreamController<UpdatePasswordEvent> _onUpdatePasswordController =
       new StreamController();
 
   @Input()
-  String username = '';
+  String username;
 
   @Input()
-  String password = '';
+  String password;
 
   @Input()
-  ViAuthClient client;
+  String errorMessage;
 
   @Input()
   bool showForgotPassword = true;
-
-  /// This is the message which will be sent to the user by email when attempting
-  /// recover its password.
-  /// %token% placeholders are replaced by a new valid token
-  /// %password% placeholders are replaced by a new auto-generated password
-  /// %username% placeholders are replaced by the users username
-  // = 'token: %token%, username/password: %username% / %password%';
-  @Input()
-  String recoverPasswordMessageEmail;
-
-  @Input()
-  String recoverPasswordSubjectEmail = 'Recover your password';
-
-  @Input()
-  String recoverPasswordFromEmail;
-
-  // = 'token: %token%, username/password: %username% / %password%';
-  @Input()
-  String recoverPasswordMessageSMS;
-
-  @Input()
-  String recoverPasswordFromSMS;
 
   @Input()
   String titleImageUrl;
@@ -165,5 +114,13 @@ class FoLoginComponent implements OnInit, OnDestroy {
   String altUrlTitle;
 
   @Output('login')
-  Stream<Map<String, String>> get onLoginOutput => _onLoginController.stream;
+  Stream<LoginEvent> get onLoginOutput => _onLoginController.stream;
+
+  @Output('recoverPassword')
+  Stream<String> get onRecoverPasswordOutput =>
+      _onRecoverPasswordController.stream;
+
+  @Output('updatePassword')
+  Stream<UpdatePasswordEvent> get onUpdatePasswordOutput =>
+      _onUpdatePasswordController.stream;
 }
