7ffb1d103cca31183e09c19b1de1e2f7a22666d6
Patrick Minogue <patrick.minogue@gmail.com>
2019-04-04 16:01:27 +0200

    Label now whitespace

--- lib/components/fo_label/fo_label_component.dart
+++ lib/components/fo_label/fo_label_component.dart
@@ -3,6 +3,7 @@ import 'package:angular/angular.dart';
 @Component(
     selector: 'fo-label',
     templateUrl: 'fo_label_component.html',
+    styleUrls: ['fo_label_component.css'],
     directives: [NgClass, NgIf])
 class FoLabelComponent {
   @Input()
