fadae9b9120e9497c05626960b84a331c14d61af
Patrick Minogue <patrick.minogue@gmail.com>
2019-04-16 17:40:23 +0200

    DropdownComponent now working

--- lib/components/fo_dropdown/fo_dropdown_component.dart
+++ lib/components/fo_dropdown/fo_dropdown_component.dart
@@ -7,15 +7,9 @@ import 'package:angular/angular.dart';
     selector: 'fo-dropdown',
     templateUrl: 'fo_dropdown_component.html',
     styleUrls: ['fo_dropdown_component.css'],
-    directives: [
-      NgClass,
-      NgFor,
-      NgIf,
-      NgStyle
-    ],
+    directives: [NgClass, NgFor, NgIf, NgStyle],
     pipes: [])
-class FoDropdownComponent
-    implements AfterViewInit, AfterChanges, OnDestroy {
+class FoDropdownComponent implements AfterViewInit, AfterChanges, OnDestroy {
   @Input()
   num width;
 
@@ -23,18 +17,22 @@ class FoDropdownComponent
   bool visible = false;
 
   @Input()
-  bool constrainToViewPort = true;
+  int offsetTop = 0;
 
+  @Input()
+  bool constrainToViewPort = true;
 
   StreamSubscription<html.MouseEvent> _onDocumentClickListener;
   final html.Element host;
-
+  final ChangeDetectorRef _changeDetectorRef;
   final StreamController _visibleController = StreamController<bool>();
-  
+  StreamSubscription<html.Event> _onDocumentScrollListener;
+  StreamSubscription<html.Event> _onWindowResizeListener;
+
   String elementMaxHeight = '100px';
   String top;
 
-  FoDropdownComponent(this.host) {
+  FoDropdownComponent(this.host, this._changeDetectorRef) {
     _onDocumentClickListener = html.document.onClick.listen((event) {
       if (visible && !_visibleController.isClosed) {
         _visibleController.add(false);
@@ -51,24 +49,31 @@ class FoDropdownComponent
   void ngAfterChanges() {
     if (visible == true) {
       final rect = host.getBoundingClientRect();
-      top = '${rect.top}px';
+      top = '${rect.top + offsetTop}px';
 
       elementMaxHeight = constrainToViewPort == true
-          ? '${html.window.innerHeight - rect.top}px'
-          : '${html.document.body.clientHeight - (rect.top + html.window.scrollY)}px';      
+          ? '${html.window.innerHeight - (rect.top + offsetTop)}px'
+          : '${html.document.body.clientHeight - (rect.top + offsetTop + html.window.scrollY)}px';
     }
   }
 
   @override
   void ngAfterViewInit() {
-    html.document.onScroll.forEach((e) {
-      top = '${host.getBoundingClientRect().top}px';      
+    html.document.onScroll.listen((e) {
+      top = '${host.getBoundingClientRect().top + offsetTop}px';
+      _changeDetectorRef.markForCheck();
+    });
+    html.window.onResize.listen((e) {
+      top = '${host.getBoundingClientRect().top + offsetTop}px';
+      _changeDetectorRef.markForCheck();
     });
   }
 
   @override
   void ngOnDestroy() {
-    _visibleController.close();    
+    _visibleController.close();
+    _onDocumentScrollListener?.cancel();
+    _onWindowResizeListener?.cancel();
     _onDocumentClickListener?.cancel();
   }
 }
