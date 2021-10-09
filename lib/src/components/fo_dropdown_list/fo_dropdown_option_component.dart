a67d963a272af44f9ca443eb60f8eb7babe56fb8
Patrick Minogue <patrick.minogue@gmail.com>
2021-10-09 15:20:09 +0200

    minor

--- lib/src/components/fo_dropdown_list/fo_dropdown_option_component.dart
+++ lib/src/components/fo_dropdown_list/fo_dropdown_option_component.dart
@@ -7,7 +7,7 @@ import '../../../fo_dropdown_option.dart';
     selector: 'fo-dropdown-option',
     templateUrl: 'fo_dropdown_option_component.html',
     styleUrls: ['fo_dropdown_option_component.css'],
-    directives: [FoIconComponent, NgClass, NgIf],
+    directives: [FoIconComponent, coreDirectives],
     changeDetection: ChangeDetectionStrategy.OnPush)
 class FoDropdownOptionComponent {
   @Input()
