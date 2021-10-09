13c927701bd526219a579b44037dd57f4478ffc1
Patrick Minogue <patrick.minogue@gmail.com>
2021-10-09 16:45:57 +0200

    added skeleton loadindicator

---
+++ lib/components/fo_load_indicator/fo_load_indicator_component.dart
@@ -0,0 +1,9 @@
+import 'package:angular/angular.dart';
+
+@Component(
+  selector: 'fo-load-indicator',
+  templateUrl: 'fo_load_indicator_component.html',
+  styleUrls: ['fo_load_indicator_component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+)
+class FoLoadIndicatorComponent {}
