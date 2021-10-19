07875720a996f85905eebe36221d81ec8a83d703
Pontus Larsson <pontus.larsson.torner@gmail.com>
2021-10-19 21:27:04 +0200

    updated fo_panel_component

--- lib/components/fo_panel/fo_panel_component.dart
+++ lib/components/fo_panel/fo_panel_component.dart
@@ -1,4 +1,3 @@
-import 'dart:async';
 import 'package:angular/angular.dart';
 import 'package:fo_components/components/fo_button/fo_button_component.dart';
 import '../../pipes/capitalize_pipe.dart';
@@ -7,30 +6,31 @@ import '../../pipes/capitalize_pipe.dart';
     selector: 'fo-panel',
     templateUrl: 'fo_panel_component.html',
     styleUrls: ['fo_panel_component.css'],
-    directives: [FoButtonComponent, NgIf],
-    pipes: [CapitalizePipe])
-class FoPanelComponent implements OnDestroy {
-  final StreamController<bool> _visibleChangeController =
-      StreamController<bool>();
+    directives: [
+      FoButtonComponent,
+      NgIf,
+    ],
+    pipes: [CapitalizePipe],
+    changeDetection: ChangeDetectionStrategy.OnPush)
 
+/// Panel component
+class FoPanelComponent {
   FoPanelComponent();
 
+  /// Header to the panel
   @Input()
   String? header;
 
+  /// Description of component
+
   @Input()
-  bool visible = true;
+  String? description;
 
-  @Output('visibleChange')
-  Stream<bool> get visibleChange => _visibleChangeController.stream;
+  /// Show expanded panel
+  @Input()
+  bool expanded = false;
 
-  void onClose() {
-    _visibleChangeController.add(false);
-    visible = false;
-  }
-
-  @override
-  void ngOnDestroy() {
-    _visibleChangeController.close();
+  void toggleExpanded() {
+    expanded = !expanded;
   }
 }
