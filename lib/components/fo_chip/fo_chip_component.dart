bbf0863aa90d30ced968ac07aac3e8b0dfcaa86e
Pontus Larsson <pontus.larsson.torner@gmail.com>
2021-10-28 18:08:01 +0200

    chip close-icon

--- lib/components/fo_chip/fo_chip_component.dart
+++ lib/components/fo_chip/fo_chip_component.dart
@@ -1,14 +1,17 @@
 import 'package:angular/angular.dart';
+import 'package:fo_components/components/fo_icon/fo_icon_component.dart';
 
 @Component(
   selector: 'fo-chip',
   templateUrl: 'fo_chip_component.html',
   styleUrls: ['fo_chip_component.css'],
-  directives: [coreDirectives],
+  directives: [coreDirectives, FoIconComponent],
   changeDetection: ChangeDetectionStrategy.OnPush,
 )
 class FoChipComponent {
   // Add name/tag to chip
   @Input()
   String? name = 'Chip';
+
+  String myCloseIcon = 'close-outline';
 }
