d81db192c870ca4866ab2742c232f6fc8668b399
Patrick Minogue <patrick.minogue@gmail.com>
2021-11-10 10:02:13 +0100

    capitalize pipe now accepts null input

--- lib/pipes/capitalize_pipe.dart
+++ lib/pipes/capitalize_pipe.dart
@@ -4,8 +4,14 @@ import 'package:angular/angular.dart';
 /// patrick --> Patrick
 @Pipe('capitalize')
 class CapitalizePipe {
-  String transform(String value) =>
-      value.isEmpty ? value : '${value[0].toUpperCase()}${value.substring(1)}';
+  String transform(String? value) {
+    if (value == null) {
+      return '';
+    }
+    return value.isEmpty
+        ? value
+        : '${value[0].toUpperCase()}${value.substring(1)}';
+  }
 
   bool supports(Object obj) => obj is String;
 }
